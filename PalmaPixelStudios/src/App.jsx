c4c9ee9a3fadbcdd1e7b9fe9097f0dbcab82ad52
Valery Gonzalez <valgonzr@gmail.com>
2024-05-24 11:57:05 -0400

    first commit

---
+++ PalmaPixelStudios/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <h1>PALMA PIXEL STUDIOS</h1>
+     <p>Coming Soon</p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
